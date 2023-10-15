31fc0ebc0dc3195b7993b7f7f3d56515895c0b3d
Brandon Nguyen <Bnguye76@students.kennesaw.edu>
2023-10-15 17:27:57 -0400

    image on history

--- rhel-history.php
+++ rhel-history.php
@@ -29,26 +29,24 @@
         <h2>Early Days: Pre-RHEL Era</h2>
         <p>
             Before the emergence of Red Hat Enterprise Linux (RHEL), enterprise Linux operating systems were fragmented, lacking a unified, standardized solution. This period was marked by the absence of a comprehensive enterprise-grade Linux distribution.
-            <img src="path-to-image/pre-rhel-era.jpg" alt="Illustration of the pre-RHEL era" class="rhel-diagram">
         </p>
 
         <h2>2000: Birth of Red Hat Enterprise Linux</h2>
         <p>
             In the year 2000, Red Hat introduced Red Hat Enterprise Linux (RHEL), revolutionizing the enterprise Linux landscape. RHEL brought forth a subscription-based, enterprise-grade Linux OS that offered stability, support, and a standardized platform for businesses.
-            <img src="path-to-image/birth-of-rhel.jpg" alt="RHEL's initial release and impact" class="rhel-diagram">
+            <img src="https://cdn.ttgtmedia.com/rms/onlineimages/condensed_history_of_red_hat-f_mobile.png" alt="RHEL's initial release and impact" class="rhel-diagram">
         </p>
 
         <h2>Adoption and Influence: RHEL's Impact</h2>
         <p>
             Rapid adoption of RHEL across industries, enterprises, and government organizations solidified its status as a cornerstone of modern IT infrastructure. RHEL played a pivotal role in advancing open-source technologies and standardizing enterprise computing.
             <a href="https://www.redhat.com/en/about/blog/next-20-years-red-hat-enterprise-linux">Learn more about RHEL's 20 years of innovation</a>
-            <img src="rhel-impact.jpg" alt="RHEL's influence on enterprise IT" class="rhel-diagram">
+            <img src="https://techaisle.com/images/RedHatCloudServices.png alt="RHEL's influence on enterprise IT" class="rhel-diagram">
         </p>
 
         <h2>Today: RHEL as a Leading Enterprise OS</h2>
         <p>
             Today, Red Hat Enterprise Linux remains a prominent, battle-tested enterprise operating system. Supported by a thriving open-source community, RHEL serves as the foundation for modern IT solutions, including cloud computing, containers, and more.
-            <img src="modern-rhel-architecture.jpg" alt="Modern RHEL architecture and deployments" class="rhel-diagram">
         </p>
 
         <footer>
--- rhel-overview.php
+++ rhel-overview.php
@@ -25,12 +25,12 @@
             ?>
         </p>
         <h1>Welcome to Red Hat Enterprise Linux (RHEL)</h1>
-        <img src="https://developers.redhat.com/sites/default/files/styles/article_feature/public/2021_RHEL_9_Featured_Article_image_A.webp?itok=N07doe4e" alt="RHEL Logo" style="width: 200px; height: 150px;" class="rhel-logo">
+        <img src="https://developers.redhat.com/sites/default/files/styles/article_feature/public/2021_RHEL_9_Featured_Article_image_A.webp?itok=N07doe4e" alt="RHEL Logo" style="width: 400px; height: 200px;" class="rhel-logo">
         <h2>What is RHEL?</h2>
         <p>Red Hat Enterprise Linux, often referred to as RHEL, is an enterprise-grade open-source operating system designed to provide a reliable and secure platform for businesses and organizations. It offers a robust foundation for a wide range of applications and services.</p>
         <h2>History</h2>
         <p>Introduced by Red Hat Inc., RHEL made its debut in the year 2000. Over the years, it has evolved into a trusted and industry-standard choice for enterprises, offering long-term support, updates, and a broad ecosystem of software solutions.</p>
-        <img src="https://linkedin.github.io/school-of-sre/level101/linux_basics/images/linux/commands/image25.png" alt="RHEL Architecture Diagram" style="width: 200px; height: 150px;" class="rhel-diagram">
+        <img src="https://linkedin.github.io/school-of-sre/level101/linux_basics/images/linux/commands/image25.png" alt="RHEL Architecture Diagram" style="width: 400px; height: 200px;" class="rhel-diagram">
         <h2>How Does it Work?</h2>
         <p>RHEL operates on various hardware platforms and cloud environments, providing a stable and secure environment for applications and data. It employs a subscription-based model that offers extensive support, security updates, and access to Red Hat's vast repository of software packages.</p>
         <p>Red Hat provides tools, resources, and services to help businesses manage, deploy, and scale their RHEL installations efficiently, ensuring high availability and security.</p>

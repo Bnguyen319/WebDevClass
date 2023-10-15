8b4074a8083ae2f76aec0bcc2e5abc8cd9aba950
Brandon Nguyen <Bnguye76@students.kennesaw.edu>
2023-10-15 17:13:14 -0400

    image size

--- rhel-overview.php
+++ rhel-overview.php
@@ -25,12 +25,12 @@
             ?>
         </p>
         <h1>Welcome to Red Hat Enterprise Linux (RHEL)</h1>
-        <img src="https://developers.redhat.com/sites/default/files/styles/article_feature/public/2021_RHEL_9_Featured_Article_image_A.webp?itok=N07doe4e" alt="RHEL Logo" class="rhel-logo">
+        <img src="https://developers.redhat.com/sites/default/files/styles/article_feature/public/2021_RHEL_9_Featured_Article_image_A.webp?itok=N07doe4e" alt="RHEL Logo" style="width: 200px; height: 150px;" class="rhel-logo">
         <h2>What is RHEL?</h2>
         <p>Red Hat Enterprise Linux, often referred to as RHEL, is an enterprise-grade open-source operating system designed to provide a reliable and secure platform for businesses and organizations. It offers a robust foundation for a wide range of applications and services.</p>
         <h2>History</h2>
         <p>Introduced by Red Hat Inc., RHEL made its debut in the year 2000. Over the years, it has evolved into a trusted and industry-standard choice for enterprises, offering long-term support, updates, and a broad ecosystem of software solutions.</p>
-        <img src="https://access.redhat.com/webassets/avalon/d/Red_Hat_OpenStack_Platform-11-Architecture_Guide-en-US/images/fce6394275bd3444892c5d3a91ccf17c/RHEL_OSP_arch_347192_1015_JCS_01_Interface-Overview.png" alt="RHEL Architecture Diagram" class="rhel-diagram">
+        <img src="https://access.redhat.com/webassets/avalon/d/Red_Hat_OpenStack_Platform-11-Architecture_Guide-en-US/images/fce6394275bd3444892c5d3a91ccf17c/RHEL_OSP_arch_347192_1015_JCS_01_Interface-Overview.png" alt="RHEL Architecture Diagram" style="width: 200px; height: 150px;" class="rhel-diagram">
         <h2>How Does it Work?</h2>
         <p>RHEL operates on various hardware platforms and cloud environments, providing a stable and secure environment for applications and data. It employs a subscription-based model that offers extensive support, security updates, and access to Red Hat's vast repository of software packages.</p>
         <p>Red Hat provides tools, resources, and services to help businesses manage, deploy, and scale their RHEL installations efficiently, ensuring high availability and security.</p>

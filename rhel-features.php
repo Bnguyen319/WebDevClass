1270f974c3359a015246b9d7f8a2c304badf9165
Brandon Nguyen <Bnguye76@students.kennesaw.edu>
2023-12-09 16:08:11 -0500

    rhel features

---
+++ rhel-features.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RHEL Features</title>
+    <link id="themeStylesheet" rel="stylesheet" href="styles.css">
+    <script src="themeChanger.js"></script>
+</head>
+<body>
+    <div class="container">
+        <nav class="navbar">
+            <a href="index.php">Home</a>
+            <a href="rhel-overview.php">RHEL Overview</a>
+            <a href="rhel-history.php">RHEL History</a>
+            <a href="rhel-features.php">RHEL Features</a>
+            <a href="Learn-More.php">Learn More</a>
+        </nav>
+        <p class="pod-name">
+            <?php
+                $pod_name = getenv('POD_NAME');
+                echo "Served by pod: $pod_name";
+            ?>
+        </p>
+        <h1>Key Features of Red Hat Enterprise Linux (RHEL)</h1>
+
+        <section>
+            <h2>Robust Security</h2>
+            <p>
+                RHEL provides a robust security framework, incorporating SELinux for mandatory access controls and features like Security Content Automation Protocol (SCAP) compliance. Regular security updates and Red Hat's commitment to security make RHEL a trusted choice for protecting sensitive data and applications.
+            </p>
+        </section>
+
+        <section>
+            <h2>Advanced Performance</h2>
+            <p>
+                RHEL is optimized for high performance, leveraging a tuned kernel, system libraries, and tools. This results in enhanced system responsiveness and efficiency, making RHEL suitable for demanding workloads in industries ranging from finance to scientific research.
+            </p>
+        </section>
+
+        <section>
+            <h2>Enterprise Support</h2>
+            <p>
+                RHEL comes with enterprise-grade support, providing access to Red Hat's vast knowledge base, expert technical support, and certified software repositories. This ensures system administrators have the assistance and resources they need to maintain a stable and reliable environment.
+            </p>
+        </section>
+
+        <section>
+            <h2>Containerization with Docker</h2>
+            <p>
+                RHEL integrates seamlessly with Docker, a leading containerization platform. Containers enable efficient application deployment, scalability, and resource utilization. With RHEL's native support for Docker, organizations can adopt a containerized approach to application development and deployment, fostering agility and collaboration.
+            </p>
+        </section>
+
+        <section>
+            <h2>Integration with Cloud Services</h2>
+            <p>
+                RHEL offers comprehensive integration with various cloud services, allowing businesses to build and manage applications in hybrid cloud environments. With RHEL, users can easily migrate workloads between on-premises data centers and public or private clouds, leveraging the flexibility and scalability of cloud computing.
+            </p>
+        </section>
+        
+        <section class="cta-section">
+            <h2>Ready to Harness RHEL's Features?</h2>
+            <p>Explore the powerful features of Red Hat Enterprise Linux and elevate your IT infrastructure. Visit our <a href="Learn-More.php">Learn More</a> section for in-depth insights!</p>
+        </section>
+    </div>
+</body>
+</html>
